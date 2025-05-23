020cc7121dc309ea172437c9f8b6f9b1071f6007
AMK1215 <aungmyokhaing1215@gmail.com>
2025-05-23 10:50:03 +0630

    player seeder update

--- app/Http/Controllers/Api/V1/Game/LaunchGameController.php
+++ app/Http/Controllers/Api/V1/Game/LaunchGameController.php
@@ -50,7 +50,7 @@ class LaunchGameController extends Controller
 
         try {
             $validatedData = $request->validate([
-                'game_code' => 'required|string',
+                //'game_code' => 'required|string',
                 'product_code' => 'required|integer',
                 'game_type' => 'required|string',
             ]);
@@ -95,7 +95,7 @@ class LaunchGameController extends Controller
             'password' => $gameProviderPassword, // <-- Use the consistent password stored in your DB
             'nickname' => $request->input('nickname') ?? $user->name,
             'currency' => $apiCurrency,
-            'game_code' => $validatedData['game_code'],
+            'game_code' => 'null',
             'product_code' => $validatedData['product_code'],
             'game_type' => $validatedData['game_type'],
             'language_code' => self::LANGUAGE_CODE,
