31e1dea20c032e2ed2ec6af4dab1b0662e873134
AMK1215 <aungmyokhaing1215@gmail.com>
2025-05-26 08:03:24 +0630

    wager-list api integrated by amk

--- resources/views/admin/wager_list/wager.blade.php
+++ resources/views/admin/wager_list/wager.blade.php
@@ -24,25 +24,25 @@
                         @if($wager)
                         <table class="table table-bordered">
                             <tbody>
-                                <tr><th>ID</th><td>{{ $wager['id'] }}</td></tr>
+                                <tr><th>ID</th><td>{{ $wager['id'] ?? '' }}</td></tr>
                                 <tr><th>Wager Code</th><td>{{ $wager['code'] ?? $wager['wager_code'] ?? '' }}</td></tr>
-                                <tr><th>Member Account</th><td>{{ $wager['member_account'] }}</td></tr>
-                                <tr><th>Round ID</th><td>{{ $wager['round_id'] }}</td></tr>
-                                <tr><th>Currency</th><td>{{ $wager['currency'] }}</td></tr>
-                                <tr><th>Provider ID</th><td>{{ $wager['provider_id'] }}</td></tr>
-                                <tr><th>Provider Line ID</th><td>{{ $wager['provider_line_id'] }}</td></tr>
-                                <tr><th>Provider Product ID</th><td>{{ $wager['provider_product_id'] }}</td></tr>
-                                <tr><th>Provider Product OID</th><td>{{ $wager['provider_product_oid'] }}</td></tr>
-                                <tr><th>Game Type</th><td>{{ $wager['game_type'] }}</td></tr>
-                                <tr><th>Game Code</th><td>{{ $wager['game_code'] }}</td></tr>
-                                <tr><th>Valid Bet Amount</th><td>{{ $wager['valid_bet_amount'] }}</td></tr>
-                                <tr><th>Bet Amount</th><td>{{ $wager['bet_amount'] }}</td></tr>
-                                <tr><th>Prize Amount</th><td>{{ $wager['prize_amount'] }}</td></tr>
-                                <tr><th>Status</th><td>{{ $wager['status'] }}</td></tr>
-                                <tr><th>Payload</th><td><pre>{{ json_encode($wager['payload'], JSON_PRETTY_PRINT) }}</pre></td></tr>
-                                <tr><th>Settled At</th><td>{{ $wager['settled_at'] ? date('m/d/Y, h:i:s A', $wager['settled_at']/1000) : '' }}</td></tr>
-                                <tr><th>Created At</th><td>{{ $wager['created_at'] ? date('m/d/Y, h:i:s A', $wager['created_at']/1000) : '' }}</td></tr>
-                                <tr><th>Updated At</th><td>{{ $wager['updated_at'] ? date('m/d/Y, h:i:s A', $wager['updated_at']/1000) : '' }}</td></tr>
+                                <tr><th>Member Account</th><td>{{ $wager['member_account'] ?? '' }}</td></tr>
+                                <tr><th>Round ID</th><td>{{ $wager['round_id'] ?? '' }}</td></tr>
+                                <tr><th>Currency</th><td>{{ $wager['currency'] ?? '' }}</td></tr>
+                                <tr><th>Provider ID</th><td>{{ $wager['provider_id'] ?? '' }}</td></tr>
+                                <tr><th>Provider Line ID</th><td>{{ $wager['provider_line_id'] ?? '' }}</td></tr>
+                                <tr><th>Provider Product ID</th><td>{{ $wager['provider_product_id'] ?? '' }}</td></tr>
+                                <tr><th>Provider Product OID</th><td>{{ $wager['provider_product_oid'] ?? '' }}</td></tr>
+                                <tr><th>Game Type</th><td>{{ $wager['game_type'] ?? '' }}</td></tr>
+                                <tr><th>Game Code</th><td>{{ $wager['game_code'] ?? '' }}</td></tr>
+                                <tr><th>Valid Bet Amount</th><td>{{ $wager['valid_bet_amount'] ?? '' }}</td></tr>
+                                <tr><th>Bet Amount</th><td>{{ $wager['bet_amount'] ?? '' }}</td></tr>
+                                <tr><th>Prize Amount</th><td>{{ $wager['prize_amount'] ?? '' }}</td></tr>
+                                <tr><th>Status</th><td>{{ $wager['status'] ?? '' }}</td></tr>
+                                <tr><th>Payload</th><td><pre>{{ isset($wager['payload']) ? json_encode($wager['payload'], JSON_PRETTY_PRINT) : '' }}</pre></td></tr>
+                                <tr><th>Settled At</th><td>{{ isset($wager['settled_at']) && $wager['settled_at'] ? date('m/d/Y, h:i:s A', $wager['settled_at']/1000) : '' }}</td></tr>
+                                <tr><th>Created At</th><td>{{ isset($wager['created_at']) && $wager['created_at'] ? date('m/d/Y, h:i:s A', $wager['created_at']/1000) : '' }}</td></tr>
+                                <tr><th>Updated At</th><td>{{ isset($wager['updated_at']) && $wager['updated_at'] ? date('m/d/Y, h:i:s A', $wager['updated_at']/1000) : '' }}</td></tr>
                             </tbody>
                         </table>
                         @else
