d601c672621a4fcd8473fbdab6187720e76d78e2
AMK1215 <aungmyokhaing1215@gmail.com>
2025-05-26 08:00:51 +0630

    wager-list api integrated by amk

--- resources/views/admin/wager_list/wager.blade.php
+++ resources/views/admin/wager_list/wager.blade.php
@@ -8,7 +8,7 @@
             </div>
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item"><a href="{{ route('wager-list') }}">Wager List</a></li>
+                    <li class="breadcrumb-item"><a href="{{ route('admin.wager-list') }}">Wager List</a></li>
                     <li class="breadcrumb-item active">Wager Detail</li>
                 </ol>
             </div>
