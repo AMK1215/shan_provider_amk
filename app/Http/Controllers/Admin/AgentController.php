d0da751be9cc561f08e0a3bff5ea7f797540fa60
AMK1215 <aungmyokhaing1215@gmail.com>
2025-05-30 07:52:24 +0630

    deposit / withdraw api updated by amk

--- app/Http/Controllers/Admin/AgentController.php
+++ app/Http/Controllers/Admin/AgentController.php
@@ -340,7 +340,7 @@ class AgentController extends Controller
     {
         $randomNumber = mt_rand(10000000, 99999999);
 
-        return 'A'.$randomNumber;
+        return 'AGENT'.$randomNumber;
     }
 
     public function banAgent($id): RedirectResponse
