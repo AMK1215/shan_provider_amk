815d741b3a5a922a0911a02a7808d880749d1f57
AMK1215 <aungmyokhaing1215@gmail.com>
2025-06-02 11:42:44 +0630

    total spin remove

--- resources/views/admin/sub_acc/agent_players.blade.php
+++ resources/views/admin/sub_acc/agent_players.blade.php
@@ -143,7 +143,7 @@
                 <i class="fas fa-right-left text-white me-1"></i>
                 Logs
             </a>
-            <a href="{{ route('admin.transferLogDetail', $player->id) }}"
+            <a href="{{ route('admin.PlayertransferLogDetail', $player->id) }}"
                 data-bs-toggle="tooltip" data-bs-original-title="Transfer Logs"
                 class="btn btn-info btn-sm">
                 <i class="fas fa-right-left text-white me-1"></i>
