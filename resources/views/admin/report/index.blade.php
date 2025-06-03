2e336fef68139d6422f43715b5ff197ad550b3b7
AMK1215 <aungmyokhaing1215@gmail.com>
2025-06-03 08:10:45 +0630

    report updated

--- resources/views/admin/report/index.blade.php
+++ resources/views/admin/report/index.blade.php
@@ -18,7 +18,7 @@
 <div class="container-fluid">
     <div class="row mb-3">
         <div class="col-12">
-            <h4 class="mb-3">Player Report Summary</h4>
+           
             <div class="row mb-3">
     <div class="col-12">
         <div id="digitalClock" class="digital-clock bg-dark text-white rounded px-3 py-2 d-inline-block shadow-sm" style="font-size:1.5rem; letter-spacing:2px;"></div>
@@ -53,7 +53,7 @@
         <div class="col-12 col-lg-11 col-xl-10">
             <div class="card shadow rounded">
                 <div class="card-header bg-light border-bottom-0">
-                    <h5 class="mb-0">Player Summary Table</h5>
+                    <h5 class="mb-0">Player Report Summary Table</h5>
                 </div>
                 <div class="card-body p-0">
                     <div class="table-responsive">
