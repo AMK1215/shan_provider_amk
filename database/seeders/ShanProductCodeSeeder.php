cde7b01c9f9002c0315073d3cefbf9e961afd2a6
AMK1215 <aungmyokhaing1215@gmail.com>
2025-06-24 10:18:20 +0630

    shan api integrated by amk

--- database/seeders/ShanProductCodeSeeder.php
+++ database/seeders/ShanProductCodeSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Admin\Product; // Make sure to import your Product model
+use App\Models\Product; // Make sure to import your Product model
 
 class ShanProductCodeSeeder extends Seeder
 {
