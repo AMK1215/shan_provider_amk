30e1e207720108b063daa1b9148448f389c25cf4
AMK1215 <aungmyokhaing1215@gmail.com>
2025-07-05 08:48:16 +0630

    agent 2d

--- app/Http/Controllers/Admin/TwoD/TwoDigitController.php
+++ app/Http/Controllers/Admin/TwoD/TwoDigitController.php
@@ -12,6 +12,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use App\Models\TwoDigit\TwoBet;
 use App\Models\TwoDigit\TwoBetSlip;
+use App\Models\TwoDigit\TwoDResult;
 
 class TwoDigitController extends Controller
 {
