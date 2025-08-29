445a24c4766ee0c987669c383aa88300edf52c75
AMK1215 <aungmyokhaing1215@gmail.com>
2025-08-29 09:30:24 +0630

    shankome url updated by amk

--- app/Http/Controllers/Api/V1/Game/LaunchGameController.php
+++ app/Http/Controllers/Api/V1/Game/LaunchGameController.php
@@ -186,8 +186,14 @@ class LaunchGameController extends Controller
         //     $validatedData['agent_code']
         // );
 
-        $launchGameUrl = sprintf(
-            ' https://shan-web-3-test.vercel.app/?user_name=%s&balance=%s',
+        // $launchGameUrl = sprintf(
+        //     ' https://shan-web-3-test.vercel.app/?user_name=%s&balance=%s',
+        //     urlencode($memberAccount),
+        //     $balance
+        // );
+
+         $launchGameUrl = sprintf(
+            'https://delight-shankomee.vercel.app//?user_name=%s&balance=%s',
             urlencode($memberAccount),
             $balance
         );
@@ -196,6 +202,7 @@ class LaunchGameController extends Controller
         //     'https://goldendragon7.pro/?user_name=%s&balance=%s',
         //     urlencode($memberAccount),
         //     $balance
+       // https://delight-shankomee.vercel.app/
         // );
 
         Log::info('Provider Launch Game URL generated', [
